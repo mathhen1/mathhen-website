f3fb907f1eb97d975e3594177f9d1aedb5ed74d5
mathen <math.z.mhj@gmail.com>
2026-07-14 14:01:17 -0300

    feat: implements intersection observer for reveal elements w/ animate

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,43 @@ import AboutMe from "./Components/AboutMe";
 import Services from "./Components/Services";
 import { useEffect, useState } from "react";
 import Intro from "./Components/Intro";
+import MoreAboutMe from "./Components/MoreAboutMe";
 
 export default function Home() {
 
   const [useIntro, setUseIntro] = useState<boolean>(true)
 
   useEffect(() => {
+
+    const root = document.querySelectorAll(".reveal")
+
+    const obs = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate-reveal")
+          console.log(entry.target.classList)
+        }
+      })
+    }, { threshold: 0.8 })
+
+    root.forEach(node => {
+      obs.observe(node)
+    })
     setTimeout(() => {
       setUseIntro(false)
-    }, 2500);
-  }, [])
+    }, 2200);
+  }, [useIntro])
 
   if (useIntro) {
     return (
       <Intro />
     )
   }
-
   return (
     <div className="relative h-full w-screen flex flex-col">
       <NavBar />
       <AboutMe />
+      <MoreAboutMe />
       <Services />
     </div>
   );
