d95a9b8b3ded4ee4e95e64204f4eb9384f4627ee
mathen <math.z.mhj@gmail.com>
2026-07-16 21:42:48 -0300

    feat(style): add animation for sublime text

--- app/Components/Intro.tsx
+++ app/Components/Intro.tsx
@@ -8,15 +8,18 @@ const Intro = () => {
         if (introRef.current) {
             setTimeout(() => {
                 introRef.current?.classList.add("animate-intro")
-            }, 1500);
+            }, 1800);
         }
     }, [])
 
     return (
-        <div className="h-screen w-screen flex items-center justify-center bg-black">
+        <div className="h-[90vh] w-screen flex items-center justify-center bg-black">
             <div ref={introRef} className="h-full w-screen flex items-center justify-center bg-black">
                 <h1 className="animate-opacity text-3xl font-black tracking-wide text-white">
-                    mathen
+                    <span className="flex flex-col gap-1 group">
+                        mathen
+                        <span className="h-px border-b-2 border-white w-0.5 animate-expansion"></span>
+                    </span>
                 </h1>
             </div>
         </div>
