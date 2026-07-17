2ba9e4e568fdfde92e52e388f30778f130545e20
mathen <math.z.mhj@gmail.com>
2026-07-16 22:18:09 -0300

    feat: create Utilities projects list

---
+++ app/Utils/projects.tsx
@@ -0,0 +1,27 @@
+import { stackList } from "./stack"
+
+const listProjects = [{
+    id: 0,
+    name: "Thays Helena - Personal",
+    desc: "Website portfolio para Thays Helena, Personal Trainer e Especialista em exericio clinico",
+    src: "website-thays.png",
+    srcMobile: "site-mobile-thays.jpg",
+    stack: [stackList[1], stackList[2], stackList[5]]
+}, {
+    id: 1,
+    name: "Ellen Souza - Promotora",
+    desc: "Website portfolio para Ellen Souza, Promotora de Eventos",
+    src: "website-ellen.png",
+    srcMobile: "site-mobile-ellen.jpg",
+    stack: [stackList[1], stackList[2], stackList[5]]
+}, {
+    id: 2,
+    name: "Matheus Henrique - Desenvolvedor",
+    desc: "Meu antigo website portfolio. Agora se tornou obseleto, já que troquei pro atual",
+    src: "website-portfolio.png",
+    srcMobile: "site-mobile-eu.jpg",
+    stack: [stackList[1], stackList[2], stackList[5]]
+}]
+
+
+export { listProjects }
