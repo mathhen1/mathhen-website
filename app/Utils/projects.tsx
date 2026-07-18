4af330babde6ae414a439238e3704c94d787feb8
mathen <math.z.mhj@gmail.com>
2026-07-18 14:41:33 -0300

    refactor: delete unused and refine some utilities

--- app/Utils/projects.tsx
+++ app/Utils/projects.tsx
@@ -5,22 +5,22 @@ const listProjects = [{
     name: "Thays Helena - Personal",
     desc: "Website portfolio para Thays Helena, Personal Trainer e Especialista em exericio clinico",
     src: "website-thays.png",
-    srcMobile: "site-mobile-thays.jpg",
-    stack: [stackList[1], stackList[2], stackList[5]]
+    stack: [stackList[1], stackList[2], stackList[5]],
+    category: "web"
 }, {
     id: 1,
     name: "Ellen Souza - Promotora",
     desc: "Website portfolio para Ellen Souza, Promotora de Eventos",
     src: "website-ellen.png",
-    srcMobile: "site-mobile-ellen.jpg",
-    stack: [stackList[1], stackList[2], stackList[5]]
+    stack: [stackList[1], stackList[2], stackList[5]],
+    category: "web"
 }, {
     id: 2,
     name: "Matheus Henrique - Desenvolvedor",
     desc: "Meu antigo website portfolio. Agora se tornou obseleto, já que troquei pro atual",
     src: "website-portfolio.png",
-    srcMobile: "site-mobile-eu.jpg",
-    stack: [stackList[1], stackList[2], stackList[5]]
+    stack: [stackList[1], stackList[2], stackList[5]],
+    category: "web"
 }]
 
 
