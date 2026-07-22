b6035d75e5367a2413b11c7e97d5f3f9ffd29a06
mathen <math.z.mhj@gmail.com>
2026-07-22 02:26:10 -0300

    style: add navbar in services page and little adjust layout

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -2,15 +2,19 @@
 
 import { ArrowLeftCircle } from "lucide-react"
 import { useRouter } from "next/navigation"
+import NavBar from "../Components/NavBar"
 
 const Services = () => {
     const router = useRouter()
     return (
-        <div className="h-screen flex items-center justify-center text-white lg:font-roboto lg:tracking-widest">
-            <ArrowLeftCircle onClick={() => router.push("/")} className="absolute top-5 left-2"/>
-            <h1 className="animate-pulse font-black text-xl lg:text-3xl">
-                Ainda em construção...
-            </h1>
+        <div className="flex flex-col text-white lg:font-roboto lg:tracking-widest">
+            <NavBar />
+            <div className="h-[90vh] relative flex flex-col items-center justify-center">
+                <ArrowLeftCircle onClick={() => router.back()} className="absolute top-2 left-2 hover:cursor-pointer"/>
+                <h1 className="animate-pulse font-black text-xl lg:text-3xl">
+                    Ainda em construção...
+                </h1>
+            </div>
         </div>
     )
 }
