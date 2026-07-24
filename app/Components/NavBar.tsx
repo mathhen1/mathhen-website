8285461e7cbf64ec87e3a09e01b975302de5b3fa
mathen <math.z.mhj@gmail.com>
2026-07-24 01:36:50 -0300

    style: add font roboto in lg+ screens

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar = () => {
     }
 
     return (
-        <div className="animate-hero relative sticky z-10 top-0 h-12 flex flex-row gap-2 bg-black text-white">
+        <div className="animate-hero lg:font-roboto relative sticky z-10 top-0 h-12 flex flex-row gap-2 bg-black text-white">
             <div className="flex flex-row items-center w-11/12">
                 <h3 className="text-2xl font-black tracking-wider max-lg:flex-1 lg:w-1/2 pl-2 lg:pl-5 p-2">
                     <a href="/">
