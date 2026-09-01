28ff4921485a74c7dd121abef7aaa11c0816a31d
mathen <math.z.mhj@gmail.com>
2026-08-31 21:30:04 -0300

    feat: create list for technical skills

---
+++ app/Utils/skillsTec.tsx
@@ -0,0 +1,10 @@
+export const aboutSkillsTec = [{ id: 1, name: "Criação de APIs Rest, RestFul e GraphQL" },
+{ id: 2, name: "Arquitetura de Microsserviços" },
+{ id: 3, name: "Auth (Autenticação e Autorização)" },
+{ id: 4, name: "Validação de Dados" },
+{ id: 5, name: "Conteinerização com Docker" },
+{ id: 6, name: "Banco de Dados NoSQL (MongoDB)" },
+{ id: 7, name: "Banco de Dados SQL (MySQL e PostgreSQL)" },
+{ id: 8, name: "Gerenciamento de WebSockets" },
+{ id: 9, name: "Criação de RAGs com LLMs" }
+]
