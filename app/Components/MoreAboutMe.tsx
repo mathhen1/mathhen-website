2040cfedb8214fd80e7bdb81c260ecf0381cadda
mathen <math.z.mhj@gmail.com>
2026-08-31 21:34:47 -0300

    refactor: convert the listing logic into a component

--- app/Components/MoreAboutMe.tsx
+++ app/Components/MoreAboutMe.tsx
@@ -29,23 +29,11 @@ const MoreAboutMe = () => {
                 </div>
 
                 <span className="lg:hidden reveal opacity-0 w-10/12 border-t border-gray-400 h-px"></span>
-
-                <div className="flex flex-col items-center lg:items-start gap-5 lg:w-1/2">
-
-                    <h2 className="reveal opacity-0 text-xl border-b-2 font-bold lg:border-b-0 lg:self-start lg:ml-2 lg:text-3xl lg:pl-2 lg:border-l-3 lg:mt-6 lg:mb-3">
-                        Tecnologias
-                    </h2>
-
-                    <div className="grid grid-cols-5 sm:grid-cols-7 lg:grid-cols-6 w-11/12 sm:w-3/4 lg:w-4/5 gap-2">
-                        {stackList.map(skill => (
-                            <div key={skill.id} className="reveal opacity-0 border-2 p-2 pb-1 flex flex-col justify-center gap-2 items-center">
-                                <img src={skill.src} alt="" className="reveal opacity-0 " />
-                                <p className="reveal opacity-0 text-[clamp(0.5rem,2vw,1rem)] sm:text-xs">{skill.title}</p>
-                            </div>
-                        ))}
-                    </div>
-
-                </div>
+                
+                <h2 className="reveal opacity-0 text-xl border-b-2 font-bold lg:border-b-0 lg:self-start lg:ml-2 lg:text-3xl lg:pl-2 lg:border-l-3 lg:mt-6 lg:mb-3">
+                    Tecnologias
+                </h2>
+                <ListTech />
 
                 <span className="lg:hidden w-10/12 border-t-2 border-gray-400 h-px mb-10"></span>
 
@@ -55,3 +43,20 @@ const MoreAboutMe = () => {
 }
 
 export default MoreAboutMe
+
+export const ListTech = () => {
+    return (
+        <div className="flex flex-col items-center lg:items-start gap-5 lg:w-1/2">
+
+            <div className="grid grid-cols-5 sm:grid-cols-7 lg:grid-cols-6 w-11/12 sm:w-3/4 lg:w-4/5 gap-2">
+                {stackList.map(skill => (
+                    <div key={skill.id} className="reveal opacity-0 border-2 p-2 pb-1 flex flex-col justify-center gap-2 items-center">
+                        <img src={skill.src} alt="" className="reveal opacity-0 " />
+                        <p className="reveal opacity-0 text-[clamp(0.5rem,2vw,1rem)] sm:text-xs">{skill.title}</p>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
+}
